8eb86d7c97b209711cf21d63ffd297e2f46d0dd0
Stephan Wendel <me@stephanwe.de>
2022-01-03 22:40:12 +0100

    feature(rtsp)!: replaced v4l2rtspserver with rtsp-simple-server
    Currently WIP, not working!. Only preparation for change.
    
    BREAKING CHANGE: v4l2rtspserver will not longer function!
    
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- libs/rtspsimple.sh
+++ libs/rtspsimple.sh
@@ -16,32 +16,43 @@
 # Exit upon Errors
 set -e
 
-function run_rtsp {
-    local cam_section rtsp_bin device port resolution fps custom
+function prepare_rtsp {
+    local cam_section rtsp_bin ffmpeg_bin
     local start_param
     cam_section="${1}"
-    rtsp_bin="$(whereis v4l2rtspserver | awk '{print $2}')"
+    rtsp_bin="bin/rtsp-simple-server/rtsp-simple-server"
+    ffmpeg_bin="$(whereis ffmpeg | awk '{print $2}')"
     device="$(get_param "cam ${cam_section}" device)"
     port=$(get_param "cam ${cam_section}" port)
     resolution=$(get_param "cam ${cam_section}" resolution)
     fps=$(get_param "cam ${cam_section}" max_fps)
-    custom="$(get_param "cam ${cam_section}" custom_flags 2> /dev/null)"
-    check_section "${cam_section}"
     split_res="$(echo "${resolution}" | \
         awk -F 'x' '{print "-W "$1 " -H "$2}')"
     start_param=(
                     -I 0.0.0.0 -P "${port}" "${split_res}" -F "${fps}" \
                     "${device}"
                 )
-    # Custom Flag Handling
-    if [ -n "${custom}" ]; then
-        start_param+=("${custom}")
-    fi
-    log_msg "Starting v4l2rtspserver with Device ${device} ..."
+    log_msg "Starting rtsp-simple-server with Device ${device} ..."
     echo "Parameters: ${start_param[*]}" | \
-    log_output "v4l2rtspserver [cam ${cam_section}]"
-
+    log_output "rtsp-simple-server [cam ${cam_section}]"
+    # Start ffmpeg
     echo "${start_param[*]}" | xargs "${rtsp_bin}" 2>&1 | \
     log_output "v4l2rtspserver [cam ${cam_section}]"
     log_msg "ERROR: Start of v4l2rtspserver [cam ${cam_section}] failed!"
+}
+
+function run_rtsp {
+    local rtsp_bin config
+    rtsp_bin="bin/rtsp-simple-server/rtsp-simple-server"
+    config="${BASE_CN_PATH}/file_templates/crowsnest-rtsp.yml"
+    log_msg "Starting rtsp-simple-server with config ${config} ..."
+    echo "Config file: ${config}" | \
+    log_output "rtsp-simple-server [cam ${cam_section}]"
+    # Start rtsp-simple-server
+    "${rtsp_bin}" "${config}" 2>&1 | \
+    log_output "v4l2rtspserver [cam ${cam_section}]"
+    # Should not be seen else failed.
+    log_msg "ERROR: Start of rtsp-simple failed!"
+    echo "Config file: ${config}" | \
+    log_output "rtsp-simple-server [cam ${cam_section}]"
 }
