886c1ef12e547420de5c32d5cafdb40e371edef0
Stephan Wendel <me@stephanwe.de>
2022-01-03 16:25:21 +0100

    refactor(messages.sh): changed deprecated_msg_1
    * Changed message according to new behavior. Will exit if missing.
    
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- libs/messages.sh
+++ libs/messages.sh
@@ -38,5 +38,5 @@ function help_msg {
 function deprecated_msg_1 {
     log_msg "Parameter 'streamer' is deprecated!"
     log_msg "Please use mode: (mjpg/webrtc)"
-    log_msg "WARN: Switching to fallback mode 'mjpg'"
+    log_msg "ERROR: Please update your webcam.conf! Stopped."
 }
