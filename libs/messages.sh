67c32d95f4b91efac1d2ab5b2ffff060934d80d9
Stephan Wendel <me@stephanwe.de>
2022-03-07 19:22:22 +0100

    fix(messages): Typo in deprecated_msg_1
    Used mode: [ ustreamer | rtsp ] instead of correct
    mode: [ mjpg | rtsp ]
    
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- libs/messages.sh
+++ libs/messages.sh
@@ -37,7 +37,7 @@ function help_msg {
 
 function deprecated_msg_1 {
     log_msg "Parameter 'streamer' is deprecated!"
-    log_msg "Please use mode: [ ustreamer | rtsp ]"
+    log_msg "Please use mode: [ mjpg | rtsp ]"
     log_msg "ERROR: Please update your webcam.conf! Stopped."
 }
 
