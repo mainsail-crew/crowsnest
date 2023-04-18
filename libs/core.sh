4124e65ec44885654d3cdafae66efc0d5010632f
Stephan Wendel <me@stephanwe.de>
2023-04-18 17:31:46 +0200

    feat(logging): extend error trace in logging
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- libs/core.sh
+++ libs/core.sh
@@ -33,8 +33,14 @@ trap 'err_exit $? $LINENO' ERR
 # Print Error Code and Line to Log
 # and kill running jobs
 function err_exit {
+    local file_trace func_trace line_trace
+    read -r LINE FUNC FILE < <(caller 0)
+    func_trace="${FUNC}"
+    file_trace="$(basename "${FILE}")"
+    line_trace="${LINE}"
     if [ "${1}" != "0" ]; then
-        log_msg "ERROR: Error ${1} occured on line ${2}"
+        log_msg "ERROR: Error ${1} occured on line ${line_trace}"
+        log_msg "==> Error occured in file: ${file_trace} -> ${func_trace}"
         log_msg "ERROR: Stopping $(basename "$0")."
         log_msg "Goodbye..."
     fi
