c48c026ccc9b197358b8cb0150bebecac6b00987
Stephan Wendel <me@stephanwe.de>
2023-05-29 11:55:33 +0200

    fix: fix error in check_legacy_raspicam
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- libs/hwhandler.sh
+++ libs/hwhandler.sh
@@ -132,7 +132,7 @@ check_legacy_raspicam() {
         if [[ "${is_mmal}" = "1" ]]; then
             mmal_error_msg
         fi
-        if [[ "${is_mmal}" = "0" ]]; then
+        if [[ "${is_startx}" = "1" ]] && [[ "${is_mmal}" = "0" ]]; then
             legacy_stack_msg
         fi
     fi
