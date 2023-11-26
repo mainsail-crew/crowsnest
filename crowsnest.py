a61eecf2834783eafc18bb51e450486b0d79a6a6
Patrick Gehrsitz <mryel00.github@gmail.com>
2023-11-26 19:33:23 +0100

    chore: wip

--- crowsnest.py
+++ crowsnest.py
@@ -42,10 +42,10 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
-logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')
-logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+#logging.debug('This message should go to the log file')
+#logging.info('So should this')
+#logging.warning('And this, too')
+#logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
 
 print(crowsnest.name)
 
