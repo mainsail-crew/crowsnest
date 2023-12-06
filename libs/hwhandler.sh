54fa21094babab5ffc50e4e69c0617cdb5efe80b
Stephan Wendel <me@stephanwe.de>
2023-12-06 10:54:00 +0100

    refactor: reuse is_raspberry_pi func
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- libs/hwhandler.sh
+++ libs/hwhandler.sh
@@ -86,8 +86,7 @@ get_libcamera_path() {
 # Determine connected "legacy" device
 function detect_legacy {
     local avail
-    if [[ -f /proc/device-tree/model ]] &&
-    grep -q "Raspberry" /proc/device-tree/model &&
+    if [[ "$(is_raspberry_pi)" = "1" ]] &&
     command -v vcgencmd &> /dev/null; then
         if vcgencmd get_camera &> /dev/null ; then
             avail="$(vcgencmd get_camera \
