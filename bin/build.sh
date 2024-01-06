db6e02ed975d723dc4ff82e383ac6681f3bdedf8
Patrick Gehrsitz <mryel00.github@gmail.com>
2024-01-06 21:38:31 +0100

    chore: shallow clone submodules

--- bin/build.sh
+++ bin/build.sh
@@ -130,7 +130,7 @@ clone_cstreamer() {
     git clone "${CROWSNEST_CAMERA_STREAMER_REPO_SHIP}" \
         -b "${CROWSNEST_CAMERA_STREAMER_REPO_BRANCH}" \
         "${BASE_CN_BIN_PATH}"/"${CSTREAMER_PATH}" \
-        "${CLONE_FLAGS[@]}" --recursive
+        "${CLONE_FLAGS[@]}" --recurse-submodules --shallow-submodules
 }
 
 ### Clone Apps
