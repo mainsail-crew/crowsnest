5410a9e4ffecbca0fbbc0520b721e47ecd4b01f7
Stephan Wendel <me@stephanwe.de>
2024-01-31 19:53:55 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- libs/hwhandler.sh
+++ libs/hwhandler.sh
@@ -85,13 +85,15 @@ get_libcamera_path() {
 
 # print libcamera resolutions
 list_picam_resolution() {
-    local device prefix
-    device="${1}"
-    prefix="$(date +'[%D %T]') crowsnest:"
-    log_msg "'libcamera' device(s) resolution(s) :"
-    while read -r i; do
-        printf "%s\t\t%s\n" "${prefix}" "${i}" >>"${CROWSNEST_LOG_PATH}"
-    done < <(libcamera-hello --list-cameras | sed '1,2d')
+
+    log_msg "HEYYY"
+    # local device prefix
+    # device="${1}"
+    # prefix="$(date +'[%D %T]') crowsnest:"
+    # log_msg "'libcamera' device(s) resolution(s) :"
+    # while read -r i; do
+    #     printf "%s\t\t%s\n" "${prefix}" "${i}" >>"${CROWSNEST_LOG_PATH}"
+    # done < <(libcamera-hello --list-cameras | sed '1,2d')
 }
 
 # Determine connected "legacy" device
