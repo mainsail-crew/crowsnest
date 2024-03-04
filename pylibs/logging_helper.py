b64295f28cdd41345ab1a4a5a244c5f5c4e6ea4f
Patrick Gehrsitz <mryel00.github@gmail.com>
2024-03-04 18:16:29 +0100

    chore: wip
    Signed-off-by: Patrick Gehrsitz <mryel00.github@gmail.com>

--- pylibs/logging_helper.py
+++ pylibs/logging_helper.py
@@ -21,9 +21,9 @@ def log_config(config_path):
     with open(config_path, 'r') as file:
         config_txt = file.read()
         # Remove comments
-        config_txt = re.sub(r'#.*$', "", config_txt, flags=re.MULTILINE)
+        config_txt = re.sub(r'\s*$', "", config_txt, flags=re.MULTILINE)
+        config_txt = re.sub(r'(\[.*\])$', "\n\\1", config_txt, flags=re.MULTILINE)
         config_txt = config_txt.strip()
-        config_txt = config_txt.replace('\n\n', '\n')
         # Split the config file into lines
         log_multiline(config_txt, log_info, indentation)
 
