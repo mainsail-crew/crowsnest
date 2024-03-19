127b0fcd6f271e8c0daf3ae566271b4fcff6b3c4
Stephan Wendel <me@stephanwe.de>
2024-03-19 20:32:56 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- libs/hwhandler.sh
+++ libs/hwhandler.sh
@@ -103,8 +103,7 @@ get_libcamera_controls() {
         sed 's/device//g;/^SNAPSHOT/q' | sed '/^SNAPSHOT/d' | \
         sed '/^CAMERA/d;/- property/d' | sed '/camera-streamer Version:/d' | \
         sed 's/- available option: //g' | sed '/^$/d;' | \
-        sed 's/([0-9]*[a-z,0-9]\,//g' | \
-        sed '/type=7/d;s#type=1\)\:\(#(bool)#g'
+        sed 's/([0-9]*[a-z,0-9]\,//g'
     else
         log_msg "WARN: 'libcamera' device option can not be displayed, because"
         log_msg "WARN: camera-streamer is not installed"
