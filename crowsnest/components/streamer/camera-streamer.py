903ae494bdb79ee03ced7b222a92397cf33939f4
Patrick Gehrsitz <github@mryel.de>
2026-03-27 16:53:49 +0100

    chore(camera-streamer): remove old comments
    Signed-off-by: Patrick Gehrsitz <github@mryel.de>

--- crowsnest/components/streamer/camera-streamer.py
+++ crowsnest/components/streamer/camera-streamer.py
@@ -62,10 +62,8 @@ class Camera_Streamer(Streamer):
             for ctrl in v4l2ctl.split(","):
                 streamer_args += [f"--camera-options={ctrl.strip()}"]
 
-        # if device.startswith('/base') and 'i2c' in device:
         if isinstance(cam, camera.Libcamera):
             streamer_args += ["--camera-type=libcamera", "--camera-format=YUYV"]
-        # elif device.startswith('/dev/video') or device.startswith('/dev/v4l'):
         elif isinstance(cam, (camera.UVC, camera.Legacy)):
             streamer_args += ["--camera-type=v4l2"]
             if cam.has_mjpg_hw_encoder():
