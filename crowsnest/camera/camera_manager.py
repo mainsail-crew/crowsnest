509db89a14862a777b5d3d6456cfb943220c0ae6
Patrick Gehrsitz <github@mryel.de>
2026-03-27 11:10:03 +0100

    refactor(camera_manager): refactor camera_manager cameras list
    Signed-off-by: Patrick Gehrsitz <github@mryel.de>

--- crowsnest/camera/camera_manager.py
+++ crowsnest/camera/camera_manager.py
@@ -7,30 +7,21 @@
 #### This File is distributed under GPLv3
 ####
 
-from typing import Optional
+from typing import List, Optional
 
 from .camera import Camera
 
-
-def get_all_cameras() -> list:
-    global cameras
-    try:
-        cameras
-    except NameError:
-        cameras = []
-    return cameras
+_cameras: List[Camera] = []
 
 
 def get_cam_by_path(path: str) -> Optional[Camera]:
-    global cameras
-    for camera in get_all_cameras():
+    for camera in _cameras:
         if camera.path_equals(path):
             return camera
     return None
 
 
-def init_camera_type(obj: Camera) -> list:
-    global cameras
+def init_camera_type(obj: Camera) -> List[Camera]:
     cams = obj.init_camera_type()
-    get_all_cameras().extend(cams)
+    _cameras.extend(cams)
     return cams
