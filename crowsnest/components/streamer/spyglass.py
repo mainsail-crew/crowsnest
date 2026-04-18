81581b06a1dd0eb5b59c0b66dbc47c6d72d43062
Patrick Gehrsitz <github@mryel.de>
2026-04-19 00:16:46 +0200

    fix(spyglass): fix some errors
    Signed-off-by: Patrick Gehrsitz <github@mryel.de>

--- crowsnest/components/streamer/spyglass.py
+++ crowsnest/components/streamer/spyglass.py
@@ -85,16 +85,16 @@ class Spyglass(Streamer):
         return process
 
     def check_config_section(self, config_section: SectionProxy) -> bool:
-        height, width = self.parameters["resolution"]
+        width, height = self.parameters["resolution"]
         custom_flags = self.parameters["custom_flags"].split()
         sw_encoding = "-sw" in custom_flags or "--use-sw-encoding" in custom_flags
         check_res = not utils.is_pi5() and not sw_encoding
-        if check_res and int(height) > 1920 and int(width) > 1080:
-            self.log_warn(
+        if check_res and int(width) > 1920 and int(height) > 1080:
+            self.log_warning(
                 "Potential crash detected. Please reduce resolution to 1920x1080 or "
                 "lower to stay within hardware constraints."
             )
-        super().check_config_section(config_section)
+        return super().check_config_section(config_section)
 
 
 def load_streamer() -> tuple[list[str], list[str]]:
